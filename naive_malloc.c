12069065eebc4f5afd3eea8f293374d95ff059f4
matternstsr <matternstsr@gmail.com>
2024-09-10 15:04:03 -0500

    so lost with the page size

--- naive_malloc.c
+++ naive_malloc.c
@@ -15,7 +15,7 @@ void *naive_malloc(size_t size)
     if (size == 0)
         return NULL;
 
-    /* Align size to the next 8-byte boundary, including the size of the block header */
+    /* Align size to the next 8-byte boundary */
     aligned_size = ((size + sizeof(Block) + 7) & ~7);
 
     if (heap_end == NULL)
