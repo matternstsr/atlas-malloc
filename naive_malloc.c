8106f7e60026a743d669bcde33a8fb876c491c5e
matternstsr <matternstsr@gmail.com>
2024-09-10 14:18:25 -0500

    so lost with the page size

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,5 +1,9 @@
 #include "malloc.h"
 
+size_t align_size(size_t size) {
+    return ((size + PAGE_SIZE - 1) / PAGE_SIZE) * PAGE_SIZE;
+}
+
 /**
  * naive_malloc - Allocates memory in the heap
  * @size: size of memory to allocate
@@ -20,7 +24,9 @@ void *naive_malloc(size_t size)
         return NULL;
 
     /* Align size to the next page boundary */
-    aligned_size = ALIGN_SIZE(size + sizeof(size_t));
+    aligned_size = align_size(size + sizeof(size_t));
+    printf("Requested size: %zu\n", size);
+    printf("Aligned size: %zu\n", aligned_size);
 
     /* Initialize heap_end if this is the first call */
     if (heap_end == NULL)
@@ -31,19 +37,20 @@ void *naive_malloc(size_t size)
     }
 
     prev_heap_end = heap_end;
+    printf("Previous heap end: %p\n", prev_heap_end);
 
     /* Extend heap if not enough space */
     if (sbrk(aligned_size) == (void *)-1)
         return NULL; /* sbrk failed */
     heap_end = (char *)heap_end + aligned_size; /* Update heap_end */
 
+    printf("New heap end after sbrk: %p\n", heap_end);
+
     /* Store block size at the beginning of the block */
     *(size_t *)prev_heap_end = aligned_size;
     
     /* Return pointer to the memory after the block header */
-    /* ptr = (char *)prev_heap_end + sizeof(size_t); */
-    ptr = (char *)prev_heap_end + (sizeof(size_t));
-    /* ptr = (char *)prev_heap_end; */
-    
+    ptr = (char *)prev_heap_end + sizeof(size_t);
+
     return ptr;
 }
