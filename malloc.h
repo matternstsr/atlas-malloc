c6e2181ca8e7cee0eb3591966e264d553e9019f7
matternstsr <matternstsr@gmail.com>
2024-09-10 14:13:46 -0500

    so lost with the page size

--- malloc.h
+++ malloc.h
@@ -13,7 +13,7 @@
 /* #define ALIGN_SIZE(size) (((size) + sizeof(Block) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1)) */
 
 /* Make the "size" to the next size up of PAGE_SIZE */
-#define ALIGN_SIZE(size) (((size) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
+#define ALIGN_SIZE(size) (((size) + 4096 - 1) & ~(4096 - 1))
 
 
 /* Struct for memory blocks */
