deee86adb2ff361b463165f92419ed08a8eeaa79
matternstsr <matternstsr@gmail.com>
2024-09-10 18:39:29 -0500

    readme

--- malloc.h
+++ malloc.h
@@ -1,32 +1,30 @@
 #ifndef MALLOC_H
 #define MALLOC_H
-
 #include <unistd.h>
 #include <stddef.h>
 #include <stdint.h>
 
-/* Define page size */
+/* start with 4096 page size */
 #define PS 4096
 
-/* Align size to be a multiple of PS, plus the header size */
+/* Make the "size" to the next size up of PS */
 #define ALIGN_SIZE(size) (((size) + sizeof(Block) + PS - 1) & ~(PS - 1))
 
-/**
-* struct Block - stores metadata for each memory block
-* @size: total number of bytes allocated for the block (including header)
-* @next: number of bytes used by the user (excluding header)
-*/
+/* Struct for memory blocks */
 typedef struct Block
 {
-	size_t size;          /* Size of the block, including the header */
-	struct Block *next;  /* Pointer to the next free block in the free list */
+	size_t size;
+	struct Block *next;
 } Block;
 
-/* Head of the free list */
+/* free_list */
 extern Block *free_list;
 
-/* Function prototypes */
 void *naive_malloc(size_t size);
-void naive_free(void *ptr);
+void *_malloc(size_t size);
+void _free(void *ptr);
 
 #endif /* MALLOC_H */
+
+
+
