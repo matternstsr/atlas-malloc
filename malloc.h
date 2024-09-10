9b37ffe7053c70e2be05b9c56e617d158715bc5a
matternstsr <matternstsr@gmail.com>
2024-09-10 13:15:07 -0500

    oops.

--- malloc.h
+++ malloc.h
@@ -11,7 +11,7 @@
 /* #define ALIGN_SIZE(size) (((size) + sizeof(Block) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1)) */
 
 /* Make the "size" to the next size up of PAGE_SIZE */
-#define ALIGN_SIZE(size) (((size) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
+#define ALIGN_SIZE(size) (((size) + 7) / 8) * 8
 
 
 /* Struct for memory blocks */
