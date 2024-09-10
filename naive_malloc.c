83a07ab2ab9070cf50d20b9ff46bfe3f3ab3e799
matternstsr <matternstsr@gmail.com>
2024-09-10 13:04:24 -0500

    oops.

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,8 +1,12 @@
 #include "malloc.h"
 
 static void *heap_end = NULL;
-static Block *free_list = NULL;
 
+/**
+ * naive_malloc - Allocates memory in the heap
+ * @size: size of memory to allocate
+ * Return: returns a pointer to the allocated memory
+ */
 void *naive_malloc(size_t size)
 {
     void *prev_heap_end;
@@ -11,50 +15,21 @@ void *naive_malloc(size_t size)
 
     if (size == 0)
         return NULL;
-
-    /* Align size to the next page boundary, including the size of the block header */
-    aligned_size = ALIGN_SIZE(size + sizeof(Block));
-
-    /* Initialize the heap if it is the first call */
+    aligned_size = ALIGN_SIZE(size + sizeof(size_t));
     if (heap_end == NULL)
     {
         heap_end = sbrk(0);
         if (heap_end == (void *)-1)
-            return NULL; /* sbrk failed */
-
-        /* Add initial heap space */
+            return NULL;
         if (sbrk(PAGE_SIZE) == (void *)-1)
-            return NULL; /* sbrk failed */
-
+            return NULL;
         heap_end = sbrk(0);
     }
-
     prev_heap_end = heap_end;
-    /* Extend heap if there is not enough space */
     if (sbrk(aligned_size) == (void *)-1)
-        return NULL; /* sbrk failed
- */
-    heap_end = (char *)heap_end + aligned_size; /* Update heap_end */
-
-    /* Initialize the new block */
-    Block *new_block = (Block *)prev_heap_end;
-    new_block->size = size;
-    new_block->next = NULL;
-
-    /* Return the memory after the block header */
-    ptr = (char *)new_block + sizeof(Block);
+        return NULL;
+    heap_end = (char *)heap_end + aligned_size;
+    *(size_t *)prev_heap_end = size;
+    ptr = (char *)prev_heap_end + sizeof(size_t);
     return ptr;
-}
-
-void _free(void *ptr)
-{
-    if (ptr == NULL)
-        return;
-
-    /* Get the block header */
-    Block *block = (Block *)((char *)ptr - sizeof(Block));
-
-    /* Add the block to the free list */
-    block->next = free_list;
-    free_list = block;
 }
