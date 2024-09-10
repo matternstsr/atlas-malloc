e596478945a9c776eed2eea239c09b3587e944f6
matternstsr <matternstsr@gmail.com>
2024-09-10 14:56:14 -0500

    so lost with the page size

--- malloc.h
+++ malloc.h
@@ -4,6 +4,25 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/* start with 4096 page size */
+#define PAGE_SIZE 4096
+
+/* Make the "size" to the next size up of PAGE_SIZE */
+#define ALIGN_SIZE(size) (((size) + sizeof(Block) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
+
+/* Struct for memory blocks */
+typedef struct Block {
+    size_t size;
+    struct Block *next;
+} Block;
+
+/* free_list */
+extern Block *free_list;
+
+void *naive_malloc(size_t size);
+void *_malloc(size_t size);
+void _free(void *ptr);
+
 #endif /* MALLOC_H */
 
 
--- naive_malloc.c
+++ naive_malloc.c
@@ -7,12 +7,32 @@
  */
 void *naive_malloc(size_t size)
 {
-    
+    static void *heap_end = NULL;
+    void *prev_heap_end;
+    void *ptr;
+    size_t aligned_size;
+
+    if (size == 0)
+        return NULL;
+
+    /* Align size to the next page boundary, including the size of the block header */
+    aligned_size = ALIGN_SIZE(size + sizeof(Block));
+
+    if (heap_end == NULL)
+        heap_end = sbrk(0);  /* Initialize heap_end to the current end of the heap */
+
+    prev_heap_end = heap_end;
+    if (sbrk(aligned_size) == (void *)-1)
+        return NULL;  /* sbrk failed */
+
+    heap_end = (char *)heap_end + aligned_size;  /* Update the heap_end */
+
+    /* Initialize the block header */
+    Block *block = (Block *)prev_heap_end;
+    block->size = aligned_size;
+    block->next = NULL;
+
+    /* Return the memory after the block header */
+    ptr = (char *)block + sizeof(Block);
     return ptr;
 }
-
-
-
-
-
-
