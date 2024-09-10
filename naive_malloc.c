bf7c3ae8e6a35735dda5c21537436ae4073c17f2
matternstsr <matternstsr@gmail.com>
2024-09-10 13:23:30 -0500

    oops.

--- naive_malloc.c
+++ naive_malloc.c
@@ -19,15 +19,25 @@ void *naive_malloc(size_t size)
     aligned_size = ALIGN_SIZE(size + sizeof(size_t));
 
     if (heap_end == NULL)
+    {
         heap_end = sbrk(0);  /* Make the heap_end to whatever break is at this time */
+        if (heap_end == (void *)-1)
+            return NULL; /* sbrk failed */
 
+        /* Allocate initial page size if necessary */
+        if (sbrk(PAGE_SIZE) == (void *)-1)
+            return NULL; /* sbrk failed */
+
+        heap_end = sbrk(0);  /* Make the heap_end to whatever break is at this time */
+    }
     prev_heap_end = heap_end;
+    /* Make heap bigger if not enough space  */   
     if (sbrk(aligned_size) == (void *)-1)
         return NULL;  /* sbrk didnt work */
     heap_end = (char *)heap_end + aligned_size;  /* Updating the heap_end */
 
     /* Store what size is at the beginning of the block */
-    *(size_t *)prev_heap_end = aligned_size;
+    *(size_t *)prev_heap_end = size;
     
     ptr = (char *)prev_heap_end + sizeof(size_t);
     /* Store what size is at the beginning of the block */
