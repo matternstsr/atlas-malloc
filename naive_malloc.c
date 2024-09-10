06fddaebdfa1e2010ad0de021be21ef0f893d707
matternstsr <matternstsr@gmail.com>
2024-09-10 15:42:36 -0500

    so lost with the page size

--- naive_malloc.c
+++ naive_malloc.c
@@ -15,26 +15,29 @@ void *naive_malloc(size_t size)
     if (size == 0)
         return NULL;
 
-    /* Align size to the next page boundary */
+    /* Align size to the next page boundary, including the size of the block header */
     aligned_size = ALIGN_SIZE(size + sizeof(size_t));
 
-    if (heap_end == NULL) {
+    if (heap_end == NULL)
         heap_end = sbrk(0);  /* Make the heap_end to whatever break is at this time */
-    }
 
     prev_heap_end = heap_end;
-
-    /* Extend the heap */
     if (sbrk(aligned_size) == (void *)-1)
-        return NULL;  /* sbrk failed */
+        return NULL;  /* sbrk didnt work */
+    heap_end = (char *)heap_end + aligned_size;  /* Updating the heap_end */
 
-    heap_end = (char *)heap_end + aligned_size;  /* Update the heap_end */
-
-    /* Store the size of the block at the beginning */
+    /* Store what size is at the beginning of the block */
     *(size_t *)prev_heap_end = aligned_size;
-
-    /* Return a pointer to the memory area just after the header */
+    
     ptr = (char *)prev_heap_end + sizeof(size_t);
-
+    /* Store what size is at the beginning of the block */
+    /* *(size_t *)prev_heap_end = aligned_size; */
+    /*  moved  */
     return ptr;
 }
+
+
+
+
+
+
