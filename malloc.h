a07f39dae5f44936e390f3463c0c8e9ee9a2b4fa
matternstsr <matternstsr@gmail.com>
2024-09-10 13:16:39 -0500

    oops.

--- malloc.h
+++ malloc.h
@@ -5,13 +5,13 @@
 #include <stdint.h>
 
 /* start with 4096 page size */
-#define PAGE_SIZE 4096
+#define PAGE_SIZE 8
 
 /* Make the "size" to the next size up of PAGE_SIZE */
 /* #define ALIGN_SIZE(size) (((size) + sizeof(Block) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1)) */
 
 /* Make the "size" to the next size up of PAGE_SIZE */
-#define ALIGN_SIZE(size) (((size) + 7) / 8) * 8
+#define ALIGN_SIZE(size) (((size) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
 
 
 /* Struct for memory blocks */
