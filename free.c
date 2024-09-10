df952963dde4f46e2d4c2bf838bda81e01a96dba
matternstsr <matternstsr@gmail.com>
2024-09-10 11:13:43 -0500

    .

--- free.c
+++ free.c
@@ -1,19 +1,10 @@
 #include "malloc.h"
 
-/* Freeing memory */
-static void naive_free_internal(void *ptr, Block **free_list_ptr)
+void _free(void *ptr)
 {
     if (ptr == NULL)
         return;
-
     Block *block = (Block *)((char *)ptr - sizeof(Block));
-    block->next = *free_list_ptr;
-    *free_list_ptr = block;
-}
-
-/* Free memory */
-void _free(void *ptr)
-{
-    static Block *free_list = NULL;  /* Static var for free list */
-    naive_free_internal(ptr, &free_list);
+    block->next = free_list;
+    free_list = block;
 }
