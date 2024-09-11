03775a7f090ee33f26ee5ca965587d422e0b74b4
matternstsr <matternstsr@gmail.com>
2024-09-11 00:01:33 -0500

    test

--- malloc.h
+++ malloc.h
@@ -7,20 +7,18 @@
 /* start with 4096 page size */
 #define PS 4096
 
-
-
 /* Make the "size" to the next size up of PS */
 #define ALIGN_SIZE(size) (((size) + sizeof(Block) + PS - 1) & ~(PS - 1))
 
 /**
-* struct Block - stores metadata for each memory block
-* @size: total number of bytes allocated for the block (including header)
-* @next: number of bytes used by the user (excluding header)
-*/
+ * struct Block - stores metadata for each memory block
+ * @size: total number of bytes allocated for the block (including header)
+ * @next: pointer to the next free block in the free list
+ */
 typedef struct Block
 {
-	size_t size;          /* Size of the block, including the header */
-	struct Block *next;  /* Pointer to the next free block in the free list */
+    size_t size;          /* Size of the block, including the header */
+    struct Block *next;   /* Pointer to the next free block in the free list */
 } Block;
 
 /* Head of the free list */
@@ -28,7 +26,6 @@ extern Block *free_list;
 
 /* Function prototypes */
 void *naive_malloc(size_t size);
-void *_malloc(size_t size);
-void _free(void *ptr);
+void naive_free(void *ptr);
 
 #endif /* MALLOC_H */
--- naive_malloc.c
+++ naive_malloc.c
@@ -1,38 +1,72 @@
 #include "malloc.h"
 
+/* Initialize the free list to NULL */
+Block *free_list = NULL;
+
 /**
-* naive_malloc - Allocates memory in the heap
-* @size: size of memory to allocate
-* Return: returns a pointer to the allocated memory
-*/
+ * naive_malloc - Allocates memory in the heap
+ * @size: size of memory to allocate
+ * Return: returns a pointer to the allocated memory
+ */
 void *naive_malloc(size_t size)
 {
-	static void *heap_end;
-	void *prev_heap_end;
-	void *ptr;
-	size_t aligned_size;
+    static void *heap_end;
+    void *prev_heap_end;
+    void *ptr;
+    size_t aligned_size;
+    Block *current, *prev = NULL;
 
-	/* heap_end = NULL; */
-	if (size == 0)
-		return (NULL);
+    if (size == 0)
+        return NULL;
 
-	/* Align size to the next page boundary, with the size of the block hdr */
-	aligned_size = ALIGN_SIZE(size + sizeof(size_t));
+    aligned_size = ALIGN_SIZE(size);
 
-	if (heap_end == NULL)
-		heap_end = sbrk(0);  /* Make the heap_end to what break is right now */
+    // Search for a suitable block in the free list
+    current = free_list;
+    while (current) {
+        if (current->size >= aligned_size) {
+            // Remove the block from the free list
+            if (prev)
+                prev->next = current->next;
+            else
+                free_list = current->next;
 
-	prev_heap_end = heap_end;
-	if (sbrk(aligned_size) == (void *)-1)
-		return (NULL);  /* sbrk didnt work */
-	heap_end = (char *)heap_end + aligned_size;  /* Updating the heap_end */
+            // Return the memory block, excluding the header
+            return (char *)current + sizeof(Block);
+        }
+        prev = current;
+        current = current->next;
+    }
 
-	/* Store what size is at the beginning of the block */
-	*(size_t *)prev_heap_end = aligned_size;
+    // If no suitable block found, allocate new memory
+    if (heap_end == NULL)
+        heap_end = sbrk(0);
 
-	ptr = (char *)prev_heap_end + sizeof(size_t);
-	/* Store what size is at the beginning of the block */
-	/* *(size_t *)prev_heap_end = aligned_size; */
-	/*  moved  */
-	return (ptr);
+    prev_heap_end = heap_end;
+    if (sbrk(aligned_size) == (void *)-1)
+        return NULL;
+
+    heap_end = (char *)heap_end + aligned_size;
+
+    // Store the size at the beginning of the block
+    ((Block *)prev_heap_end)->size = aligned_size;
+    ptr = (char *)prev_heap_end + sizeof(Block);
+
+    return ptr;
+}
+
+/**
+ * naive_free - Frees memory previously allocated by naive_malloc
+ * @ptr: pointer to the memory to free
+ */
+void naive_free(void *ptr)
+{
+    if (!ptr)
+        return;
+
+    Block *block_to_free = (Block *)((char *)ptr - sizeof(Block));
+
+    // Add the block to the beginning of the free list
+    block_to_free->next = free_list;
+    free_list = block_to_free;
 }
