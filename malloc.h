ddfe325a19593486957af59a9496bfa18ba528d8
matternstsr <matternstsr@gmail.com>
2024-09-10 23:34:00 -0500

    test

--- malloc.h
+++ malloc.h
@@ -1,6 +1,12 @@
 #ifndef MALLOC_NEW_H
 #define MALLOC_NEW_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdint.h>
+#include <string.h>
 #include <stddef.h> // For size_t
 
 // Initialize the allocator
@@ -8,5 +14,26 @@ void init_allocator();
 
 // Allocate memory of a given size
 void *naive_malloc(size_t size);
+
+// Page size
+#define PAGE_SIZE 4096
+
+// Metadata size (size_t header)
+#define METADATA_SIZE sizeof(size_t)
+
+// Define a struct to manage allocated blocks of the same size
+typedef struct Block {
+    struct Block *next;
+    void *memory;
+} Block;
+
+// Define a struct for each size class
+typedef struct SizeClass {
+    Block *free_list;
+    size_t size;
+} SizeClass;
+
+// Maximum number of size classes we will handle
+#define MAX_SIZE_CLASSES 128
 
 #endif // MALLOC_NEW_H
--- naive_malloc.c
+++ naive_malloc.c
@@ -1,31 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <stdint.h>
-#include <string.h>
 #include "malloc.h"
-
-// Page size
-#define PAGE_SIZE 4096
-
-// Metadata size (size_t header)
-#define METADATA_SIZE sizeof(size_t)
-
-// Define a struct to manage allocated blocks of the same size
-typedef struct Block {
-    struct Block *next;
-    void *memory;
-} Block;
-
-// Define a struct for each size class
-typedef struct SizeClass {
-    Block *free_list;
-    size_t size;
-} SizeClass;
-
-// Maximum number of size classes we will handle
-#define MAX_SIZE_CLASSES 128
 
 // Array of size classes
 static SizeClass size_classes[MAX_SIZE_CLASSES];
