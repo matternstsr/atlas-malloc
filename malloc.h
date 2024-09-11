572b0bf1d1741bb9f2c0ff0e4a8632972cb38cf5
matternstsr <matternstsr@gmail.com>
2024-09-10 23:48:22 -0500

    test

--- malloc.h
+++ malloc.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 
 /* start with 4096 page size */
-#define PS 8
+#define PS 1
 
 /* Make the "size" to the next size up of PS */
 #define ALIGN_SIZE(size) (((size) + sizeof(Block) + PS - 1) & ~(PS - 1))
