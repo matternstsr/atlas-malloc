821391bdcb13a2f416b9a84284c78543b266ab29
matternstsr <matternstsr@gmail.com>
2024-09-11 00:22:33 -0500

    test

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,38 +1,62 @@
 #include "malloc.h"
 
+/* Define the start of the heap */
+void *start_of_heap = NULL;
+
 /**
-* naive_malloc - Allocates memory in the heap
-* @size: size of memory to allocate
-* Return: returns a pointer to the allocated memory
-*/
+ * naive_malloc - Allocates memory in the heap
+ * @size: size of memory to allocate
+ * Return: returns a pointer to the allocated memory
+ */
 void *naive_malloc(size_t size)
 {
-	static void *heap_end;
-	void *prev_heap_end;
-	void *ptr;
-	size_t aligned_size;
+    static void *heap_end;
+    void *prev_heap_end;
+    void *ptr;
+    size_t aligned_size;
+    size_t space_available;
 
-	/* heap_end = NULL; */
-	if (size == 0)
-		return (NULL);
+    if (size == 0)
+        return (NULL);
 
-	/* Align size to the next page boundary, with the size of the block hdr */
-	aligned_size = ALIGN_SIZE(size + sizeof(size_t));
+    /* Align size to the next page boundary, including the size of the block header */
+    aligned_size = ALIGN_SIZE(size + sizeof(size_t));
 
-	if (heap_end == NULL)
-		heap_end = sbrk(0);  /* Make the heap_end to what break is right now */
+    if (heap_end == NULL)
+    {
+        /* Initialize heap_end and start_of_heap */
+        heap_end = sbrk(0);
+        if (start_of_heap == NULL) 
+        {
+            start_of_heap = heap_end;  /* Set start_of_heap to the initial break */
+        }
+    }
 
-	prev_heap_end = heap_end;
-	if (sbrk(aligned_size) == (void *)-1)
-		return (NULL);  /* sbrk didnt work */
-	heap_end = (char *)heap_end + aligned_size;  /* Updating the heap_end */
+    /* Check if there's enough space at the beginning of the heap */
+    space_available = (size_t)((char *)heap_end - (char *)start_of_heap);
+    if (space_available >= aligned_size)
+    {
+        /* Use the space from the beginning of the heap */
+        prev_heap_end = start_of_heap;
+        start_of_heap = (char *)start_of_heap + aligned_size;  /* Update start_of_heap */
 
-	/* Store what size is at the beginning of the block */
-	*(size_t *)prev_heap_end = aligned_size;
+        /* Store the size at the beginning of the block */
+        *(size_t *)prev_heap_end = aligned_size;
 
-	ptr = (char *)prev_heap_end + sizeof(size_t);
-	/* Store what size is at the beginning of the block */
-	/* *(size_t *)prev_heap_end = aligned_size; */
-	/*  moved  */
-	return (ptr);
+        ptr = (char *)prev_heap_end + sizeof(size_t);
+        return (ptr);
+    }
+    
+    /* If not enough space at the beginning, use sbrk to extend the heap */
+    prev_heap_end = heap_end;
+    if (sbrk(aligned_size) == (void *)-1)
+        return (NULL);  /* sbrk didn't work */
+
+    heap_end = (char *)heap_end + aligned_size;  /* Update heap_end */
+
+    /* Store the size at the beginning of the block */
+    *(size_t *)prev_heap_end = aligned_size;
+
+    ptr = (char *)prev_heap_end + sizeof(size_t);
+    return (ptr);
 }
