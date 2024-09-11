52b40dd1102dce248ef8cb8f5c6379281d534dfe
matternstsr <matternstsr@gmail.com>
2024-09-10 23:34:47 -0500

    test

--- naive_malloc.c
+++ naive_malloc.c
@@ -31,7 +31,7 @@ void *naive_malloc(size_t size) {
     }
 
     size_t total_size = size + METADATA_SIZE;
-    size_t aligned_size = ((total_size + PAGE_SIZE - 1) / PAGE_SIZE) * PAGE_SIZE;
+    /* size_t aligned_size = ((total_size + PAGE_SIZE - 1) / PAGE_SIZE) * PAGE_SIZE; */
 
     SizeClass *size_class = get_size_class(size);
     if (size_class == NULL) {
