b478b83536575976ad6f091cbcd4890ed077c798
matternstsr <matternstsr@gmail.com>
2024-09-10 23:52:48 -0500

    test

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,59 +1,38 @@
 #include "malloc.h"
 
-Block *free_list = NULL;
-
+/**
+* naive_malloc - Allocates memory in the heap
+* @size: size of memory to allocate
+* Return: returns a pointer to the allocated memory
+*/
 void *naive_malloc(size_t size)
 {
-    if (size == 0)
-        return NULL;
+	static void *heap_end;
+	void *prev_heap_end;
+	void *ptr;
+	size_t aligned_size;
 
-    Block *prev = NULL;
-    Block *current = free_list;
-    size_t aligned_size = ALIGN_SIZE(size);
+	/* heap_end = NULL; */
+	if (size == 0)
+		return (NULL);
 
-    // Search the free list for a suitable block
-    while (current != NULL)
-    {
-        if (current->size >= aligned_size)
-        {
-            if (current->size > aligned_size + sizeof(Block))
-            {
-                // Split the block
-                Block *new_block = (Block *)((char *)current + aligned_size);
-                new_block->size = current->size - aligned_size;
-                new_block->next = current->next;
-                current->size = aligned_size;
-                current->next = new_block;
-            }
-            if (prev)
-                prev->next = current->next;
-            else
-                free_list = current->next;
+	/* Align size to the next page boundary, with the size of the block hdr */
+	aligned_size = ALIGN_SIZE(size + sizeof(size_t));
 
-            return (char *)current + sizeof(size_t);
-        }
-        prev = current;
-        current = current->next;
-    }
+	if (heap_end == NULL)
+		heap_end = sbrk(0);  /* Make the heap_end to what break is right now */
 
-    // Request more memory from the system
-    void *ptr = sbrk(aligned_size);
-    if (ptr == (void *)-1)
-        return NULL;
+	prev_heap_end = heap_end;
+	if (sbrk(aligned_size) == (void *)-1)
+		return (NULL);  /* sbrk didnt work */
+	heap_end = (char *)heap_end + aligned_size;  /* Updating the heap_end */
 
-    Block *new_block = (Block *)ptr;
-    new_block->size = aligned_size;
-    new_block->next = NULL;
+	/* Store what size is at the beginning of the block */
+	*(size_t *)prev_heap_end = aligned_size;
 
-    return (char *)new_block + sizeof(size_t);
-}
-
-void _free(void *ptr)
-{
-    if (!ptr)
-        return;
-
-    Block *block = (Block *)((char *)ptr - sizeof(size_t));
-    block->next = free_list;
-    free_list = block;
+	ptr = (char *)prev_heap_end + sizeof(size_t);
+	/* Store what size is at the beginning of the block */
+	/* *(size_t *)prev_heap_end = aligned_size; */
+	/*  moved  */
+	return (ptr);
 }
