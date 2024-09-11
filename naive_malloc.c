6f4ce202b16714648d7fb06023b6401bf85491ed
matternstsr <matternstsr@gmail.com>
2024-09-10 23:44:10 -0500

    test

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,38 +1,69 @@
 #include "malloc.h"
 
+/* Initialize the free list head */
+Block *free_list = NULL;
+
 /**
-* naive_malloc - Allocates memory in the heap
-* @size: size of memory to allocate
-* Return: returns a pointer to the allocated memory
-*/
+ * naive_malloc - Allocates memory in the heap
+ * @size: size of memory to allocate
+ * Return: returns a pointer to the allocated memory
+ */
 void *naive_malloc(size_t size)
 {
-	static void *heap_end;
-	void *prev_heap_end;
-	void *ptr;
-	size_t aligned_size;
+    if (size == 0)
+        return NULL;
 
-	/* heap_end = NULL; */
-	if (size == 0)
-		return (NULL);
+    Block *current = free_list;
+    Block *prev = NULL;
+    size_t aligned_size = ALIGN_SIZE(size);
 
-	/* Align size to the next page boundary, with the size of the block hdr */
-	aligned_size = ALIGN_SIZE(size + sizeof(size_t));
+    /* Search for a suitable free block */
+    while (current != NULL)
+    {
+        if (current->size >= aligned_size)
+        {
+            if (current->size > aligned_size + sizeof(Block))
+            {
+                /* Split the block */
+                Block *new_block = (Block *)((char *)current + aligned_size);
+                new_block->size = current->size - aligned_size;
+                new_block->next = current->next;
+                current->size = aligned_size;
+                current->next = new_block;
+            }
+            if (prev)
+                prev->next = current->next;
+            else
+                free_list = current->next;
 
-	if (heap_end == NULL)
-		heap_end = sbrk(0);  /* Make the heap_end to what break is right now */
+            return (char *)current + sizeof(size_t);
+        }
+        prev = current;
+        current = current->next;
+    }
 
-	prev_heap_end = heap_end;
-	if (sbrk(aligned_size) == (void *)-1)
-		return (NULL);  /* sbrk didnt work */
-	heap_end = (char *)heap_end + aligned_size;  /* Updating the heap_end */
+    /* No suitable block found, request more memory from the OS */
+    void *ptr = sbrk(aligned_size);
+    if (ptr == (void *)-1)
+        return NULL;
 
-	/* Store what size is at the beginning of the block */
-	*(size_t *)prev_heap_end = aligned_size;
+    Block *new_block = (Block *)ptr;
+    new_block->size = aligned_size;
+    new_block->next = NULL;
 
-	ptr = (char *)prev_heap_end + sizeof(size_t);
-	/* Store what size is at the beginning of the block */
-	/* *(size_t *)prev_heap_end = aligned_size; */
-	/*  moved  */
-	return (ptr);
+    return (char *)new_block + sizeof(size_t);
+}
+
+/**
+ * _free - Frees the allocated memory
+ * @ptr: pointer to the memory to free
+ */
+void _free(void *ptr)
+{
+    if (!ptr)
+        return;
+
+    Block *block = (Block *)((char *)ptr - sizeof(size_t));
+    block->next = free_list;
+    free_list = block;
 }
