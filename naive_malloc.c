99f2efbf76e670b08e76b5d9f1e6a5df03e6eaa9
matternstsr <matternstsr@gmail.com>
2024-09-12 10:53:41 -0500

    naive still not working - different approach

--- naive_malloc.c
+++ naive_malloc.c
@@ -10,29 +10,30 @@ void *naive_malloc(size_t size)
 	static void *heap_end;
 	void *prev_heap_end;
 	void *ptr;
-	size_t aligned_size;
+	size_t aligned_sz;
+    size_t aligned_sz = ((size + 7) / 8) * 8;
 
 	/* heap_end = NULL; */
 	if (size == 0)
 		return (NULL);
 
 	/* Align size to the next page boundary, with the size of the block hdr */
-	aligned_size = ALIGN_SIZE(size + sizeof(size_t));
+	aligned_sz = ALIGN_SIZE(size + sizeof(size_t));
 
 	if (heap_end == NULL)
 		heap_end = sbrk(0);  /* Make the heap_end to what break is right now */
 
 	prev_heap_end = heap_end;
-	if (sbrk(aligned_size) == (void *)-1)
+	if (sbrk(aligned_sz) == (void *)-1)
 		return (NULL);  /* sbrk didnt work */
-	heap_end = (char *)heap_end + aligned_size;  /* Updating the heap_end */
+	heap_end = (char *)heap_end + aligned_sz;  /* Updating the heap_end */
 
 	/* Store what size is at the beginning of the block */
-	*(size_t *)prev_heap_end = aligned_size;
+	*(size_t *)prev_heap_end = aligned_sz;
 
 	ptr = (char *)prev_heap_end + sizeof(size_t);
 	/* Store what size is at the beginning of the block */
-	/* *(size_t *)prev_heap_end = aligned_size; */
+	/* *(size_t *)prev_heap_end = aligned_sz; */
 	/*  moved  */
 	return (ptr);
 }
