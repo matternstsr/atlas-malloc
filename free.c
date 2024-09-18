89ff1ba8276743d716039b69f31a9545ee4a567c
matternstsr <matternstsr@gmail.com>
2024-09-18 15:37:40 -0500

    betty

--- free.c
+++ free.c
@@ -1,17 +1,21 @@
 #include "malloc.h"
 
 /**
- * Frees the allocated memory pointed to by ptr.
- * This function adds the block back to the free list.
- *
- * @param ptr Pointer to the memory block to be freed.
- */
+* _free - Frees the allocated memory pointed to by ptr.
+*
+* This function adds the block back to the free list.
+*
+* @ptr: Pointer to the memory block to be freed.
+*/
 void _free(void *ptr)
 {
-    if (ptr == NULL)
-        return;
+	free_list;
+	Block *block;
 
-    Block *block = (Block *)((char *)ptr - sizeof(Block));
-    block->next = free_list;
-    free_list = block;
+	if (ptr == NULL)
+		return;
+
+	block = (Block *)((char *)ptr - sizeof(Block));
+	block->next = free_list;
+	free_list = block;
 }
