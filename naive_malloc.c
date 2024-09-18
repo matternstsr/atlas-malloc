29ae612e5d8f292e45264c6ed936c944569ace25
matternstsr <matternstsr@gmail.com>
2024-09-18 14:00:05 -0500

    testing

--- naive_malloc.c
+++ naive_malloc.c
@@ -6,57 +6,56 @@ static n_heap_t heap = {NULL, 0, 0, 0};
  * naive_malloc - Allocates memory in the heap
  * @size: size of memory to allocate
  * Return: returns a pointer to the allocated memory
-*/
+ */
+void *naive_malloc(size_t size) {
+    if (size == 0) return NULL;
 
-void *naive_malloc(size_t size)
-{
-	static int flag;
-	size_t aligned_sz = ((size + 7) / 8) * 8;
-	n_header_t *ptr = NULL;
+    // Align the requested size
+    size_t aligned_size = (size + sizeof(n_header_t) + 7) & ~7; // Round up to nearest multiple of 8
 
-	if (!flag)
-	{
-		heap.first_block = sbrk(0);
-		while (heap.heap_size < (aligned_sz + 8))
-		{
-			sbrk(getpagesize());
-			heap.heap_size += getpagesize(), heap.heap_free += getpagesize();
-		}
-		heap.first_block->total_bytes = aligned_sz;
-		heap.heap_free = heap.heap_size - (8 + aligned_sz);
-		heap.total_blocks = 1;
-		ptr = heap.first_block + 1;
-		flag = 1;
-		return ((void *) ptr);
-	}
-	while ((aligned_sz + 8) > heap.heap_free)
-	{
-		sbrk(getpagesize());
-		heap.heap_size += getpagesize(), heap.heap_free += getpagesize();
-	}
-	ptr = n_move_block((8 + aligned_sz));
-	heap.total_blocks++;
-	heap.heap_free -= (8 + aligned_sz);
-	return ((void *) ++ptr);
-}
+    // First allocation
+    if (heap.first_block == NULL) {
+        heap.first_block = sbrk(aligned_size + sizeof(n_header_t));
+        if (heap.first_block == (void *)-1) return NULL; // sbrk failed
+        heap.first_block->total_bytes = aligned_size;
+        heap.heap_size = aligned_size + sizeof(n_header_t);
+        heap.heap_free = heap.heap_size - (aligned_size + sizeof(n_header_t));
+        heap.total_blocks = 1;
+        return (void *)((char *)heap.first_block + sizeof(n_header_t));
+    }
 
+    // Traverse the blocks to find a suitable space
+    n_header_t *current = heap.first_block;
+    for (size_t i = 0; i < heap.total_blocks; i++) {
+        size_t total_bytes = current->total_bytes;
 
-/**
- * n_move_block - Function for traversing the header blocks
- * @size: Size user requests plus block header
- * Return: pointer to big enough chunk for size
-*/
-n_header_t *n_move_block(size_t size)
-{
-	size_t i = 0, total = 0;
-	n_header_t *current;
+        // Check if this block has enough space
+        if (total_bytes >= aligned_size) {
+            // If enough space, split the block if there's leftover space
+            if (total_bytes >= aligned_size + sizeof(n_header_t) + 8) { // Ensure we can split
+                n_header_t *next_block = (n_header_t *)((char *)current + sizeof(n_header_t) + aligned_size);
+                next_block->total_bytes = total_bytes - aligned_size - sizeof(n_header_t);
+                current->total_bytes = aligned_size; // Adjust current block size
+            }
+            return (void *)((char *)current + sizeof(n_header_t)); // Return usable memory pointer
+        }
 
-	current = heap.first_block;
-	for (; i < heap.total_blocks; i++)
-	{
-		total = current->total_bytes;
-		current = (n_header_t *)((char *)current + sizeof(n_header_t) + total);
-	}
-	current->total_bytes = size - 8;
-	return (current);
+        // Move to the next block
+        current = (n_header_t *)((char *)current + sizeof(n_header_t) + total_bytes);
+    }
+
+    // No suitable block found, expand the heap
+    void *new_block = sbrk(aligned_size + sizeof(n_header_t));
+    if (new_block == (void *)-1) return NULL; // sbrk failed
+
+    // Create the new block
+    current = (n_header_t *)new_block;
+    current->total_bytes = aligned_size;
+
+    // Update heap metadata
+    heap.heap_size += aligned_size + sizeof(n_header_t);
+    heap.heap_free -= aligned_size + sizeof(n_header_t);
+    heap.total_blocks++;
+
+    return (void *)((char *)current + sizeof(n_header_t)); // Return usable memory pointer
 }
