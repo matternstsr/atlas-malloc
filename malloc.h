2cfb71cb4bedbdc6b26ed8c7672f1f7b98d204b1
matternstsr <matternstsr@gmail.com>
2024-09-18 16:36:37 -0500

    betty

--- malloc.h
+++ malloc.h
@@ -8,14 +8,7 @@
 #define PS 4096
 
 /* Make the "size" to the next size up of PS */
-/* #define ALIGN_SIZE(size) (((size) + sizeof(Block) + PS - 1) & ~(PS - 1))
- */
-#define ALIGN_SIZED(size) (((size) + 7) & ~7)  // Align to 8 bytes
-
-/* #define ALIGN_SIZE(size) (((size) + 7) & ~7)  // Align to 8 bytes
-#define BUFFER_SIZE 4096  // Total size of the buffer */
-static char buffer[BUFFER_SIZE]; // Static buffer for allocation
-/* static size_t allocated_size = 0; // Track allocated size */
+#define ALIGN_SIZE(size) (((size) + sizeof(Block) + PS - 1) & ~(PS - 1))
 
 /**
 * struct Block - stores metadata for each memory block
