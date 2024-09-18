279d60ed2035b7d6702be81810f730720f84c344
matternstsr <matternstsr@gmail.com>
2024-09-18 14:23:53 -0500

    testing

--- malloc.h
+++ malloc.h
@@ -7,92 +7,27 @@
 #include <string.h>
 #include <unistd.h>
 
-/* start with 4096 page size */
 #define PS 4096
-
-/* Make the "size" to the next size up of PS */
 #define ALIGN_SIZE(size) (((size) + sizeof(Block) + PS - 1) & ~(PS - 1))
 
-#define BLOCK_SIZE sizeof(mem_block_t)
-
-
-/**
-* struct Block - stores metadata for each memory block
-* @size: total number of bytes allocated for the block (including header)
-* @next: number of bytes used by the user (excluding header)
-*/
-typedef struct Block
-{
-	size_t size;          /* Size of the block, including the header */
-	struct Block *next;  /* Pointer to the next free block in the free list */
+typedef struct Block {
+    size_t size;
+    struct Block *next;
 } Block;
 
-/**
-* struct Block - stores metadata for each memory block
-* @size: total number of bytes allocated for the block (including header)
-* @next: number of bytes used by the user (excluding header)
-*/
-typedef struct Block_n
-{
-	size_t size;          /* Size of the block, including the header */
-} Block_n;
-
-
-/**
- * struct mem_header_s - stores header data for each block
- * @allocated_bytes: total number of bytes allocated
-*/
-typedef struct mem_header_s
-{
-    size_t allocated_bytes;
-} mem_header_t;
-
-/**
- * struct mem_block_s - stores header data for each block
- * @allocated_bytes: total number of bytes allocated
- * @used_bytes: number of bytes used
-*/
-typedef struct mem_block_s
-{
+typedef struct mem_block_s {
     size_t allocated_bytes;
     size_t used_bytes;
 } mem_block_t;
 
-/**
- * struct mem_heap_s - Struct for storing heap data
- * @initial_block: pointer to first block of metadata
- * @total_size: Total size of the heap in bytes
- * @free_size: Amount of heap free to use in bytes
- * @block_count: The total number of blocks in the heap
-*/
-typedef struct mem_heap_s
-{
+typedef struct mem_heap_s {
     mem_block_t *initial_block;
     size_t total_size;
     size_t free_size;
     size_t block_count;
 } mem_heap_t;
 
-void *simple_malloc(size_t size);
-mem_header_t *navigate_block(size_t size);
-
-void *_my_malloc(size_t size);
-mem_block_t *move_block(size_t size);
-
-void _my_free(void *ptr);
-
-/* Head of the free list */
-extern Block *free_list;
-
-/* Function prototypes */
 void *naive_malloc(size_t size);
-void *_malloc(size_t size);
-void _free(void *ptr);
-void *naive_malloc(size_t size);
-mem_header_t *navigate_block(size_t size);
+mem_block_t *navigate_block(size_t size);  // Ensure this matches
 
 #endif /* MALLOC_H */
-
-
-
-
--- naive_malloc.c
+++ naive_malloc.c
@@ -2,41 +2,37 @@
 
 static mem_heap_t memory_heap = {NULL, 0, 0, 0};
 
-/**
- * naive_malloc - Allocates memory in the heap
- * @requested_size: size of memory to allocate
- * Return: returns a pointer to the allocated memory
-*/
 void *naive_malloc(size_t requested_size)
 {
     static int initialized;
     size_t adjusted_size = ((requested_size + 7) / 8) * 8;
-    mem_block_t *block_ptr = NULL; // Change to mem_block_t
+    mem_block_t *block_ptr = NULL;
 
     if (!initialized)
     {
         memory_heap.initial_block = sbrk(0);
-        while (memory_heap.total_size < (adjusted_size + sizeof(mem_block_t))) // Update size here
+        while (memory_heap.total_size < (adjusted_size + sizeof(mem_block_t)))
         {
             sbrk(getpagesize());
             memory_heap.total_size += getpagesize();
             memory_heap.free_size += getpagesize();
         }
-        // Initialize the first block
         memory_heap.initial_block->allocated_bytes = adjusted_size;
         memory_heap.free_size = memory_heap.total_size - (sizeof(mem_block_t) + adjusted_size);
         memory_heap.block_count = 1;
-        block_ptr = memory_heap.initial_block; // No need to add 1 here, it already points to the correct location
+        block_ptr = memory_heap.initial_block;
         initialized = 1;
         return ((void *) block_ptr);
     }
+    
     while ((adjusted_size + sizeof(mem_block_t)) > memory_heap.free_size)
     {
         sbrk(getpagesize());
         memory_heap.total_size += getpagesize();
         memory_heap.free_size += getpagesize();
     }
-    block_ptr = (mem_block_t *)navigate_block((sizeof(mem_block_t) + adjusted_size));
+    
+    block_ptr = navigate_block((sizeof(mem_block_t) + adjusted_size));
     memory_heap.block_count++;
     memory_heap.free_size -= (sizeof(mem_block_t) + adjusted_size);
     return ((void *) ++block_ptr);
@@ -45,9 +41,8 @@ void *naive_malloc(size_t requested_size)
 mem_block_t *navigate_block(size_t size)
 {
     size_t index = 0, total_allocated = 0;
-    mem_block_t *current_block; // Change to mem_block_t
+    mem_block_t *current_block = memory_heap.initial_block;
 
-    current_block = memory_heap.initial_block;
     for (; index < memory_heap.block_count; index++)
     {
         total_allocated = current_block->allocated_bytes;
