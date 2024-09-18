59988b569679eff103a874db53ddfea348eb19ec
matternstsr <matternstsr@gmail.com>
2024-09-18 15:12:44 -0500

    testing

--- malloc.h
+++ malloc.h
@@ -21,40 +21,12 @@ typedef struct Block
 	struct Block *next;  /* Pointer to the next free block in the free list */
 } Block;
 
-
-/**
- * struct n_header_s - stores header data for each block
- * @total_b: total number of bytes allocated
-*/
-typedef struct n_header_s
-{
-	size_t total_b;
-} segment_header_t;
-
-/**
- * struct n_heap_s - Struct for storing heap data
- * @first_segment: pointer to first block of metadata
- * @tot_sz: Total size of the heap in bytes
- * @free_sz: Amount of heap free to use in bytes
- * @block_count: The total number of blocks in the heap
-*/
-typedef struct n_heap_s
-{
-	segment_header_t *first_segment;
-	size_t tot_sz;
-	size_t free_sz;
-	size_t block_count;
-} n_heap_t;
-
 /* Head of the free list */
 extern Block *free_list;
 
 /* Function prototypes */
 void *naive_malloc(size_t size);
 void *_malloc(size_t size);
-void _free(void *block_pointer);
-
-void *naive_malloc(size_t size);
-segment_header_t *n_move_block(size_t size);
+void _free(void *ptr);
 
 #endif /* MALLOC_H */
--- naive_malloc.c
+++ naive_malloc.c
@@ -1,63 +1,38 @@
 #include "malloc.h"
 
-static n_heap_t heap = {NULL, 0, 0, 0};
-
 /**
- * naive_malloc - Allocates memory in the heap
- * @size: size of memory to allocate
- * Return: returns a pointer to the allocated memory
+* naive_malloc - Allocates memory in the heap
+* @size: size of memory to allocate
+* Return: returns a pointer to the allocated memory
 */
-
 void *naive_malloc(size_t size)
 {
-	static int is_heap;
-	size_t aligned_sz = ((size + 7) / 8) * 8;
-	segment_header_t *block_pointer = NULL;
+	static void *heap_end;
+	void *prev_heap_end;
+	void *ptr;
+	size_t aligned_size;
 
-	if (!is_heap)
-	{
-		heap.first_segment = sbrk(0);
-		while (heap.tot_sz < (aligned_sz + 8))
-		{
-			sbrk(getpagesize());
-			heap.tot_sz += getpagesize();
-			heap.free_sz += getpagesize();
-		}
-		heap.first_segment->total_b = aligned_sz;
-		heap.free_sz = heap.tot_sz - (8 + aligned_sz);
-		heap.block_count = 1;
-		block_pointer = heap.first_segment + 1;
-		is_heap = 1;
-		return ((void *) block_pointer);
-	}
-	while ((aligned_sz + 8) > heap.free_sz)
-	{
-		sbrk(getpagesize());
-		heap.tot_sz += getpagesize(), heap.free_sz += getpagesize();
-	}
-	block_pointer = n_move_block((8 + aligned_sz));
-	heap.block_count++;
-	heap.free_sz -= (8 + aligned_sz);
-	return ((void *) ++block_pointer);
-}
+	/* heap_end = NULL; */
+	if (size == 0)
+		return (NULL);
 
+	/* Align size to the next page boundary, with the size of the block hdr */
+	aligned_size = ALIGN_SIZE(size + sizeof(size_t));
 
-/**
- * n_move_block - Function for traversing the header blocks
- * @size: Size user requests plus block header
- * Return: pointer to big enough chunk for size
-*/
-segment_header_t *n_move_block(size_t size)
-{
-    segment_header_t *cur_seg;
-    size_t index = 0, total_b = 0;
+	if (heap_end == NULL)
+		heap_end = sbrk(0);  /* Make the heap_end to what break is right now */
 
-	cur_seg = heap.first_segment;
-	for (; i < heap.block_count; i++)
-	{
-		total_b = cur_seg->total_b;
-		cur_seg = (segment_header_t *)((char *)cur_seg + sizeof(segment_header_t) + total_b);
-	}
-	cur_seg->total_b = size - 8;
-	return (cur_seg);
+	prev_heap_end = heap_end;
+	if (sbrk(aligned_size) == (void *)-1)
+		return (NULL);  /* sbrk didnt work */
+	heap_end = (char *)heap_end + aligned_size;  /* Updating the heap_end */
+
+	/* Store what size is at the beginning of the block */
+	*(size_t *)prev_heap_end = aligned_size;
+
+	ptr = (char *)prev_heap_end + sizeof(size_t);
+	/* Store what size is at the beginning of the block */
+	/* *(size_t *)prev_heap_end = aligned_size; */
+	/*  moved  */
+	return (ptr);
 }
